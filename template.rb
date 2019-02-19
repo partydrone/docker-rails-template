f90d8a8b09f8456503d9b02d288fd39431062205
Andrew Porter <partydrone@me.com>
2019-02-18 22:13:07 -0700

    Start building Rails application template

---
+++ template.rb
@@ -0,0 +1,103 @@
+# Add gems
+gem 'omniauth', '~> 1.6.1'
+gem 'omniauth-auth0', '~> 2.0.0'
+gem 'pundit'
+gem 'rolify'
+
+gem_group :development, :test do
+  gem 'pry'
+  gem 'pry-rails'
+end
+
+gem_group :test do
+  gem 'guard'
+  gem 'guard-minitest'
+  gem 'minitest-rails'
+end
+
+# Configure application
+
+# Create Docker files
+file 'Dockerfile', <<-CODE
+FROM ruby:2.6-alpine
+
+ENV APP_DIR /srv/<%= @name %>
+
+RUN apk update && apk add build-base nodejs postgresql-dev
+
+RUN mkdir ${APP_DIR}
+WORKDIR ${APP_DIR}
+
+COPY Gemfile* ./
+RUN bundle install --binstubs --system -j4
+
+COPY . .
+
+LABEL maintainer="Nick Janetakis <nick.janetakis@gmail.com>"
+
+CMD [ "puma", "-C", "config/puma.rb" ]
+
+CODE
+
+file '.dockerignore', <<-CODE
+.git*
+tmp/
+.dockerignore
+docker-compose.yml
+docker-compose.override.yml
+Dockerfile
+README.md
+
+CODE
+
+file 'docker-compose.yml', <<-CODE
+version: '3.7'
+
+services:
+  postgres:
+    image: 'postgres:10.3-alpine'
+    volumes:
+      - './tmp/data/postgres:/var/lib/postgresql/data'
+    env_file:
+      - '.env'
+
+  redis:
+    image: 'redis:4.0-alpine'
+    command: redis-server --requirepass yourpassword
+    volumes:
+      - './tmp/data/redis:/data'
+
+  app:
+    build: .
+    depends_on:
+      - 'postgres'
+      - 'redis'
+    volumes:
+      - '.:/srv/<%= @name %>'
+    env_file:
+      - '.env'
+
+  sidekiq:
+    command: sidekiq -C config/sidekiq.yml.erb
+    depends_on:
+      - 'postgres'
+      - 'redis'
+    env_file:
+      - '.env'
+    image: <%= @name %>_app:latest
+    volumes:
+      - '.:/srv/<%= @name %>'
+
+  cable:
+    command: puma -p 28080 cable/config.ru
+    depends_on:
+      - 'redis'
+    env_file:
+      - '.env'
+    image: <%= @name %>_app:latest
+    ports:
+      - '28080:28080'
+    volumes:
+      - '.:/srv/<%= @name %>'
+
+  CODE
